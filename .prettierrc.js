bd1190f1bd862f610ac0f1f975d9689fa9e62a64
Felix Martin <fmartin@bigml.com>
2021-03-02 15:00:40 +0100

    applied lint fixes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    trailingComma: 'none',
-    tabWidth: 2,
-    semi: false,
-    singleQuote: true,
-    pugSingleQuote: false,
-    printWidth: 120,
-    arrowParens: 'avoid'
-  }
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  pugSingleQuote: false,
+  printWidth: 120,
+  arrowParens: 'avoid'
+}
